a70ee5fa918f907c845773af4070b6bff2a6d4c0
Niv Badichi <nivbad88@gmail.com>
2026-01-20 11:23:33 +0200

    filled candymaker header file

---
+++ CandyMaker.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "SugarSupplier.hpp"
+#include "ChocolateSupplier.hpp"
+
+class CandyMaker : public SugarSupplier, public ChocolateSupplier {
+public:
+
+    CandyMaker(const char* sugarSupplierName, const char* chocolateSupplierName);
+    void makeCandy();
+};
